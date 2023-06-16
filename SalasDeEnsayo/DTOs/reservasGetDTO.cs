9f013cc8f8b4ae984dfcd52ffc1229ff6eef7fdd
Maximiliano Ruel <mruel@GAG.LOCAL>
2023-06-16 13:38:50 -0300

    : Please enter the commit message for your changes. Lines starting

--- SalasDeEnsayo/DTOs/reservasGetDTO.cs
+++ SalasDeEnsayo/DTOs/reservasGetDTO.cs
@@ -2,10 +2,13 @@
 {
     public class reservasGetDTO
     {
+        public int id { get; set; }
         public int userId { get; set; }
         public int salaDeEnsayoId { get; set; }
         public SalaDeEnsayoGetDTO salaDeEnsayo { get; set; }
         public DateTime fechaDesde { get; set; }
         public DateTime fechaHasta { get; set; }
+        public bool confirmado { get; set; }
+        public DateTime fechaDeConfirmacion { get; set; }
     }
 }
